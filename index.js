5fbafb7a68e6a6b1137ca595c1e4204c6e19ec31
Hailey Loechler <haileyloechler@gmail.com>
2022-12-11 07:32:16 -0800

    added messages for users to answer

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 // TODO: Include packages needed for this application
 const inquirer = require ('inquirer')
-const fs = require('fs')
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Add a description of your project'
+        },
+        {
+            type: 'list',
+            name: 'Contents',
+            message: 'Add sections for your table of contents'
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'How do you install your application?'
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message:'How do you use your application?'
+        },
+        {
+            type: 'list',
+            name: 'License',
+            message: 'Which license did you use?',
+            choices: ['MIT', 'Apache', 'GNU', 'GPL', 'N/A']
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Did anyone else contribute to the project and if so please list their names and links to GitHub?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide examples of how you ran tests on the project so others may run the same tests'
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
